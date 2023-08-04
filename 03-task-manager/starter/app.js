759d45423c5b501dc3320fa31e4a8ffa0e235a96
ehrun32 <aruncheriakara@gmail.com>
2023-08-04 12:14:58 -0400

    update the database

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -1,1 +1,9 @@
-console.log('Task Manager App')
+const express = require("express");
+const app = express();
+const tasks = "./controllers/tasks";
+
+app.use("/api/v1/tasks", tasks);
+
+const port = 3000;
+
+app.listen(port, console.log(`server is listening on port ${port}...`));
